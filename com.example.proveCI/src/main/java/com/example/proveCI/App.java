589f21dd077fd979651fc2c6d5f26a511657c508
Francesco Terrosi <francesco.terrosi1@gmail.com>
2017-11-05 17:15:05 +0100

    Still doing experiments

--- com.example.proveCI/src/main/java/com/example/proveCI/App.java
+++ com.example.proveCI/src/main/java/com/example/proveCI/App.java
@@ -4,9 +4,16 @@ package com.example.proveCI;
  * Hello world!
  *
  */
-public class App 
-{
-    public String hello() {
-    	return "HulloWorld!";
-    }
+public class App {
+	
+	public String hello(String name) {
+		if (name == null) {
+			return "Hello!";
+		} else
+			return "HelloWorld!";
+	}
+	
+	public String hello() {
+		return hello(null);
+	}
 }
