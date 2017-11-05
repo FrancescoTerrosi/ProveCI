c276851c777985a8cdf4cca39083ab8138ef9e4c
Francesco Terrosi <francesco.terrosi1@gmail.com>
2017-11-06 00:47:52 +0100

    fixed the notorius typo -.-

--- travis_build.sh
+++ travis_build.sh
@@ -2,7 +2,7 @@
 set -ev
 if [ "$WITH_COVERALLS" == "true" ];
 then
-  mvn -f com.examples.myproject/pom.xml clean verify -Pjacoco coveralls:report;
+  mvn -f com.example.proveCI/pom.xml clean verify -Pjacoco coveralls:report;
 elif [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
-  mvn -f com.examples.myproject/pom.xml clean verify;
+  mvn -f com.example.proveCI/pom.xml clean verify;
 fi
